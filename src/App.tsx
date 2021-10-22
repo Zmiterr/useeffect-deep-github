d1a720d972b40e3f44e7294b03c87b968bf06004
Zmiter <ozmiter@gmail.com>
2021-10-22 11:08:05 +0300

    feat: add basic functional with get users and users details

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { SearchResult, SearchUser, UserDetails } from "./types/types";
 
 function App() {
+  const [selectedUserData, setSelectedUserData] = useState<UserDetails | null>(
+    null
+  );
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [users, setUsers] = useState<SearchUser[]>([]);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [searchInput, setSearchInput] = useState("zmiterr");
+
+  useEffect(() => {
+    if (selectedUser) {
+      document.title = selectedUser;
+      (async function () {
+        const response = await axios.get<UserDetails>(
+          `https://api.github.com/users/${selectedUser}`
+        );
+        setSelectedUserData(response.data);
+      })();
+    }
+  }, [selectedUser]);
+
+  useEffect(() => {
+    (async function () {
+      const response = await axios.get<SearchResult>(
+        `https://api.github.com/search/users?q=${searchInput}`
+      );
+      setUsers(response.data.items);
+    })();
+    // axios
+    //   .get<SearchResult>(`https://api.github.com/search/users?q=${searchInput}`)
+    //   .then((res) => setUsers(res.data.items));
+  }, [searchInput]);
+
   return (
     <div className="App">
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchInput}
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <button>Find</button>
+        </div>
 
+        <ul>
+          {users.map((user) => (
+            <li
+              key={user.id}
+              className={selectedUser === user.login ? "selected" : ""}
+              onClick={() => {
+                setSelectedUser(user.login);
+              }}
+            >
+              {user.login}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>{selectedUserData?.login}</h2>
+        {selectedUserData && (
+          <div>
+            <img src={selectedUserData.avatar_url} alt="" />
+            <br />
+            Followers: {selectedUserData.followers} From:{" "}
+            {selectedUserData.location}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/types/types.ts
@@ -0,0 +1,23 @@
+export interface SearchResult {
+  items: SearchUser[];
+}
+
+export interface SearchUser {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  url: string;
+  html_url: string;
+  followers: number;
+}
+
+export interface UserDetails {
+  login: string;
+  id: number;
+  avatar_url: string;
+  url: string;
+  html_url: string;
+  followers: number;
+  location: string;
+}
