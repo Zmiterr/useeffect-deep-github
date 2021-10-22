4b75f56e948b68d867f3a15963e8e3b53da4398a
Zmiter <ozmiter@gmail.com>
2021-10-22 21:35:11 +0300

    feat: add timer

---
+++ src/components/user-detalis/timer/timer.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState, FC } from "react";
+
+interface TimerProps {
+  timerOut: number;
+  setTimerOut: React.Dispatch<React.SetStateAction<number>>;
+  selectedUserData: number;
+}
+
+const Timer: FC<TimerProps> = ({ timerOut, setTimerOut, selectedUserData }) => {
+  const [timer, setTimer] = useState(timerOut);
+
+  useEffect(() => {
+    setTimerOut(timerOut);
+  }, [timerOut]);
+
+  useEffect(() => {
+    setTimer(timer);
+  }, [timer]);
+
+  useEffect(() => {
+    const interval = setInterval(() => setTimerOut((prev) => prev - 1), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [selectedUserData]);
+  return <div>{timerOut}</div>;
+};
+
+export default Timer;
--- src/components/user-detalis/user-detalis.tsx
+++ src/components/user-detalis/user-detalis.tsx
@@ -1,15 +1,32 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { UserDetails } from "../../types/types";
+import Timer from "./timer/timer";
 
 interface UserDetailsBoxProps {
   selectedUserData: UserDetails;
 }
 
+const initialTimerSeconds = 10;
 const UserDetailsBox: FC<UserDetailsBoxProps> = ({ selectedUserData }) => {
-  console.log(selectedUserData);
+  const [timer, setTimer] = useState(initialTimerSeconds);
+  useEffect(() => {
+    setTimer(initialTimerSeconds);
+  }, [selectedUserData]);
+
+  useEffect(() => {
+    if (timer < 1) {
+      //TODO hide element
+    }
+  }, [timer]);
+
   return (
     <div>
       <h2>{selectedUserData?.login}</h2>
+      <Timer
+        timerOut={timer}
+        setTimerOut={setTimer}
+        selectedUserData={selectedUserData?.id}
+      />
       {selectedUserData && (
         <div>
           <img src={selectedUserData.avatar_url} alt="" />
